07ab376ddd062873bfe80c5d847e6e1a6daa130c
Reynaldo <rh3051@tc.columbia.edu>
2024-06-04 19:49:16 -0400

    Add console log statements to column and row functions

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const grid = document.querySelector('.grid');
-
 // 1) get input via a function that prompts for columns & rows 
 // 2) store the values in seperate variables 
 // 3) IF these values are both numbers
     // 3A) IF less than or equal to 100, pass these values as arguments into functions 
       // createColumns(columns)
       // createRows(rows)
+
+const grid = document.querySelector('.grid');
 
 const button = document.createElement('button');
 button.textContent = 'Click';
@@ -22,14 +22,10 @@ button.addEventListener('click', () => {
   }
 }); 
 
-/* 
-
-createColumns(x) {
-// create x number of columns
+function createColumns(x) {
+    console.log(x); 
 }
 
-createRows(y) {
-// create y number of rows
+function createRows(y) {
+    console.log(y);
 }
-
-*/
